b613f9ec53e252e9b34b1c7ffd4cd95b62194903
Bennett <32626837+Bannatte@users.noreply.github.com>
2026-04-15 18:43:43 -0700

    Implement letter printing based on user input
    Added user input for number of rows and implemented loops to print letters in a pyramid pattern.

--- main.cpp
+++ main.cpp
@@ -5,15 +5,19 @@ int main()
 {
     int n;
     // TODO: ask user for input
+    cin >> n;
 
+    char letter;
     // TODO: outer loop for each row
-
+    for (i=1; i<=5; i++){
         // TODO: inner loop to print letters for this row
-
+        for (j=1; j<=i; j++){
             // TODO: print the letter for this column
-
+            letter = 64 + j;
+            cout << letter << " ";
+        }
         // TODO: print newline after each row
-
-
+        cout << "\n";
+    }
     return 0;
 }
