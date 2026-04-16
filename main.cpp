5636c22320c4c9c0f1c7085646a47b4e13590429
Bennett <32626837+Bannatte@users.noreply.github.com>
2026-04-15 18:44:56 -0700

    Fix nested loop structure for letter printing

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main()
 
     char letter;
     // TODO: outer loop for each row
-    for (i=1; i<=5; i++){
+    for (int i=1; i<=5; i++){
         // TODO: inner loop to print letters for this row
-        for (j=1; j<=i; j++){
+        for (int j=1; j<=i; j++){
             // TODO: print the letter for this column
             letter = 64 + j;
             cout << letter << " ";
